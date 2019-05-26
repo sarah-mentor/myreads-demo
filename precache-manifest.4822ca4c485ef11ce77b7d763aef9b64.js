ff329d3129130980c28bc7b5535bf5abd0081d55
Sarah Maris <sarah@sarahmaris.com>
2019-05-26 11:09:54 -0400

    Updates

---
+++ precache-manifest.4822ca4c485ef11ce77b7d763aef9b64.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1de53a3e2be77b672e83ebb7ec754a",
+    "url": "/myreads-demo/index.html"
+  },
+  {
+    "revision": "ce7fb8a37dba4a0f8ac6",
+    "url": "/myreads-demo/static/css/main.193d0a39.chunk.css"
+  },
+  {
+    "revision": "eef91c77c220d1345a5a",
+    "url": "/myreads-demo/static/js/2.2b68f7bd.chunk.js"
+  },
+  {
+    "revision": "ce7fb8a37dba4a0f8ac6",
+    "url": "/myreads-demo/static/js/main.388129cd.chunk.js"
+  },
+  {
+    "revision": "276f65fb85817123cebf",
+    "url": "/myreads-demo/static/js/runtime~main.2af49409.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-demo/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-demo/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads-demo/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "51adb8eb8d23044d1807e76fb6dda947",
+    "url": "/myreads-demo/static/media/stacked-books.51adb8eb.jpg"
+  }
+]);
